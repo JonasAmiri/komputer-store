a9b6348c2f6596d42434d6a72bdffae5fa6b11b8
Jonas Amiri <jamiri17@student.aau.dk>
2023-01-17 20:50:47 +0100

    function done

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const laptopsElement = document.getElementById("laptops");
 const featuresElement = document.getElementById("Features")
+const descriptionElement = document.getElementById("description")
+const priceElement = document.getElementById("price")
+const titleElement = document.getElementById("title")
+const imageElement = document.getElementById("image")
+const btnWorkElement = document.getElementById("btnWork")
+const showWorkElement = document.getElementById("show")
+const showBankElement = document.getElementById("Output")
+const btnBankBalance = document.getElementById("btnBank")
+const btnLoanElement = document.getElementById("btnLoan");
+
 
 
 
@@ -29,6 +39,78 @@ const handleLaptopMenuChange = e=> {
 
 laptopsElement.addEventListener("change", handleLaptopMenuChange );
 
+
+
+
+const descriptionLaptopMenuChange = e => {
+    const selectedLaptop = laptops[e.target.selectedIndex];
+    descriptionElement.innerText = selectedLaptop.description;
+}
+
+laptopsElement.addEventListener("change", descriptionLaptopMenuChange);
+
+
+
+const priceLaptopMenuChange = e => {
+    const selectedLaptop = laptops[e.target.selectedIndex];
+    priceElement.innerText = `Price is ${selectedLaptop.price} dk`;
+}
+
+laptopsElement.addEventListener("change", priceLaptopMenuChange);
+
+const titleLaptopMenuChange = e => {
+    const selectedLaptop = laptops[e.target.selectedIndex];
+    titleElement.innerText = selectedLaptop.title;
+}
+
+laptopsElement.addEventListener("change", titleLaptopMenuChange);
+
+
+const imageLaptopMenuChange = e => {
+    const selectedLaptop = laptops[e.target.selectedIndex];
+    Element.innerText = selectedLaptop.title;
+}
+
+laptopsElement.addEventListener("change", titleLaptopMenuChange);
+
+
+showWorkElement.innerText = "0";
+
+btnWorkElement.addEventListener("click", function(){
+
+    showWorkElement.innerText=parseInt(showWorkElement.innerText)+100;
+
+   
+    
+
+});
+
+let activeLoan = false;
+
+btnBankBalance.addEventListener("click", function(){
+    if(!activeLoan){
+        showBankElement.innerText = parseInt(showWorkElement.innerText) + parseInt(showBankElement.innerText);
+        showWorkElement.innerText = 0;
+    }
+    else{
+
+    }
+})
+
+btnLoanElement.addEventListener("click", function(){
+    let loanTemp = prompt("how much would u like to Loan?");
+    console.log(sign);
+    if(!activeLoan){
+        
+    }
+
+    if(loanTemp>= parseInt(showBankElement.innerText*2)){
+        console.log("you cant not take a loan")
+    } 
+
+
+
+})
 
 
 
